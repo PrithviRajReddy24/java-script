5ee510bb5e0a90f78251fb3a1bd392de7765720a
unknown <velma.pruthvi24@gmail.com>
2024-05-21 05:24:33 +0530

    adding java script fundamentals practice file

---
+++ practice.js
@@ -0,0 +1,386 @@
+// /* let country = "India";
+// let continent = "Asia";
+// let population = 1400000000;
+// console.log(country);
+// console.log(continent);
+// console.log(population); */
+
+// /* const isLand = false;
+// const population = 1400000000;
+// const country = "India";
+// let language = "Telugu";
+
+// console.log(isLand);
+// console.log(population);
+// console.log(country);
+// console.log(language); */
+
+// /* const isLand = false;
+// let population = 33;
+// const country = "India";
+// let language = "Telugu";
+// const continent = "Asia";
+
+// console.log(population / 2);
+// population++;
+
+// console.log(population);
+// console.log(population > 6);
+// console.log(population > 33);
+
+// const description1 = `${country} is in ${continent}; and its ${population} million people speak ${language}`
+
+// console.log(description1); */
+
+// /* const country = "India";
+// let population = 22;
+// if (population > 33) {
+//     console.log(`${country}'s population is above average `);
+// } else {
+//     console.log(`${country}'s population is ${33 - population} million`);
+
+// }
+
+//  */
+
+
+// /* console.log('9' - '5'); // ->
+// console.log('19' - '13' + '17'); // -> ?
+// console.log('19' - '13' + 17); // -> ?
+// console.log('123' < 57); // -> ?
+// console.log(5 + 6 + '4' + 9 - 4 - 2); // -> ? */
+
+// //const numNeighbours = prompt("How many neighbour countries your country has");
+
+// //
+// /*
+// const numNeighbours1 = Number(prompt("How many neighbour countries your country has"));
+
+// if (numNeighbours1 === 1) {
+//     console.log("number of neighbouring countries is 1")
+// } else if (numNeighbours1 > 1) {
+//     console.log("number of neighbouring countries are greater than 1")
+// } else {
+//     console.log("no boundaries")
+// } */
+// /*
+// let language = 'hindi';
+
+// switch (language) {
+//     case 'chinese':
+//     case 'mandarine':
+//         console.log("It is the most spoken language in the world");
+//         break;
+//     case 'spanish':
+//         console.log("It is the second most spoken language in the world");
+//         break;
+//     case 'english':
+//         console.log("It is the third most spoken language in the world");
+//         break;
+//     case 'hindi':
+//         console.log("It is the fourth most spoken language in the world");
+//         break;
+//     case 'arabic':
+//         console.log("It is the fifth most spoken language in the world");
+//         break;
+//     default:
+//         console.log("Great language too");
+
+// } */
+
+// /* let population = 22;
+// const country = "India";
+// console.log(`${country}'s population is ${population > 33 ? 'above' : 'below'} average`); */
+
+// /*
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
+// //console.log(typeof true);
+// console.log(typeof javaScriptIsFun);
+// //console.log(typeof 23);
+// //console.log(typeof "Prithvi");
+// javaScriptIsFun = "Hello";
+// console.log(typeof javaScriptIsFun); */
+// /*
+// let year = 1993;
+// console.log(year);
+// console.log(typeof year);
+
+// let year1;
+// console.log(year1);
+// console.log(typeof year1);
+
+// console.log(typeof null); */
+// /*
+// let age = 30;
+// a/* ge = 31;
+
+// /* const new1 = "Helo";
+// new1 =/*  "hello";
+// var variab = "programmer";
+// variab = "Coding";
+
+//  */
+
+// /* const now = 2037;
+// const priage = now - 1993;
+// const tonyage = now - 2006;
+
+// console.log(priage, tonyage);
+// console.log(priage * 2, tonyage / 10, 2 ** 3);
+//  */
+
+// /* const firstName = "Prithviraj";
+// const lastName = "Reddy";
+// console.log(firstName + " " + lastName);
+
+// let x = 10 + 5;
+// x += 5;
+// x *= 5;
+// x++;
+// x--;
+// x--;
+// console.log(x);
+//  */
+
+// /* const now = 2037;
+// const priage = now - 1993;
+// const tonyage = now - 2006;
+// console.log(now - 1993 > now - 2006);
+// console.log(25 - 10 - 5);
+
+// let x, y;
+// x = y = 25 - 5 - 5;
+// console.log(x, y);
+// const aveAge = (priage + tonyage) / 2;
+// console.log(priage, tonyage, aveAge);
+
+//  */
+
+// /* var markWeight = 78;
+// var markHeight = 1.69;
+// var johnWeight = 92;
+// var johnHeight = 1.95;
+// //var markBMI = Math.round(markWeight / (markHeight * markHeight).toFixed(4));
+// //var johnBMI = Math.round(johnWeight / (johnHeight * johnHeight).toFixed(4));
+// //console.log(`Here markBMI ${markBMI} is higher than johnBMI ${johnBMI}`);
+// var markBMI = (markWeight / (markHeight * markHeight)).toFixed(2);
+// var johnBMI = (johnWeight / (johnHeight * johnHeight)).toFixed(2);
+// if (markBMI > johnBMI) {
+//     console.log(`mark's BMI ${markBMI} is higher than john's BMI ${johnBMI}`);
+// } else {
+//     console.log(`mark's BMI ${markBMI} is lower than john's BMI ${johnBMI}`)
+// }
+
+
+
+
+// var markWeight1 = 95;
+// var markHeight1 = 1.88;
+// var johnWeight1 = 85;
+// var johnHeight1 = 1.76;
+// var markBMI1 = (markWeight1 / (markHeight1 * markHeight1)).toFixed(2);
+// var johnBMI1 = (johnWeight1 / (johnHeight1 * johnHeight1)).toFixed(2);
+// if (markBMI1 > johnBMI1) {
+//     console.log(`mark1's BMI ${markBMI1} is higher than john1's BMI ${johnBMI1}`);
+// } else {
+//     console.log(`mark1's BMI ${markBMI1} is lower than john1's BMI ${johnBMI1}`);
+// }
+// ///console.log(`Here markBMI1 ${markBMI1} is lower than johnBMI1 ${johnBMI1}`);
+
+
+// console.log('string with\n\
+// multiple\n\
+// lines');
+// console.log(`string with
+// multiple
+// lines`);
+//  */
+
+
+// /* var newAge = 16;
+// if (newAge <= 18) {
+//     const eligibleAge = 18 - newAge;
+//     console.log(`you have to wait for another ${eligibleAge} years for driving license`)
+// } else {
+//     console.log(`you are eligible for driving license`)
+// }
+
+
+// const inputyear = 1993;
+// console.log(inputyear + 18);
+
+// const inputyear1 = '1993';
+// console.log(Number(inputyear1), inputyear1);
+// console.log(Number(inputyear1) + 18);
+
+
+// console.log(Number('Prithvi'));
+// //console.log(Number(Prithvi));
+// console.log(typeof (NaN));
+
+// let n = '1' + 1;
+// n = n - 1;
+// console.log(n); */
+
+
+// /* console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean("Tony"));
+// console.log(Boolean({}));
+
+//  */
+// //let money = 0;
+// /* let money = 100;
+// if (money) {
+//     console.log(`Donot spend it all`);
+// } else {
+//     console.log(`Get a job`)
+// }
+
+// //let height;
+// let height = 1.9;
+// if (height) {
+//     console.log("Here Height is defined")
+// } else {
+//     console.log("Height is undefined")
+// }
+
+// //let age = 18;
+// let age = '18';
+// if (age === 18) {
+//     console.log("Congrats you are elegible for griving license(strict)")
+// } else {
+//     console.log("congrats you are eligible for driving license(loose)")
+// }
+
+// //const favourite = prompt("please enter your havourite number");
+// const favourite = Number(prompt("please enter your havourite number"));
+// console.log(favourite);
+// console.log(typeof (favourite));
+
+// //if (favourite == 23)
+// if (favourite === 23) {
+//     console.log("23 is my favourite number")
+// } else if (favourite === 7) {
+//     console.log("7 is my favourite number")
+// } else if (favourite === 9) {
+//     console.log("9 is my favourite number")
+// } else {
+//     console.log("number is not 23,7 and 9")
+// } */
+
+
+// // const hasDriverLicense = true;
+// // const hasGoodVision = true;
+// // const isTired = false;
+
+// // console.log(hasDriverLicense && hasGoodVision);
+// // console.log(hasDriverLicense && hasGoodVision);
+// // console.log(!hasDriverLicense)
+
+// // const shouldDrive = hasDriverLicense && hasGoodVision
+
+// // // if (shouldDrive) {
+// // //     console.log("You are elgible to drive")
+// // // } else {
+// // //     console.log("you are not eligible to drive")
+// // // }
+// // /* const hasDriverLicense = true;
+// // const hasGoodVision = false; */
+
+// // if (hasDriverLicense && hasGoodVision && !isTired) {
+// //     console.log("you are eligible to drive")
+// // } else {
+// //     console.log("someone else should drive")
+// // }
+
+
+
+// // const scoreDolphines = (96 + 108 + 89) / 3;
+// // const scoreKoalas = (88 + 91 + 110) / 3;
+// // console.log(scoreDolphines, scoreKoalas);
+// // if (scoreDolphines > scoreKoalas) {
+// //     console.log("Dolphines win the trophy");
+// // } else if (scoreKoalas > scoreDolphines) {
+// //     console.log("Koalas win the trophy");
+// // } else if (scoreDolphines === scoreKoalas) {
+// //     console.log("Both win the trophy");
+// // }
+
+// /* const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreKoalas = (88 + 91 + 110) / 3;
+// console.log(scoreDolphins, scoreKoalas);
+
+// if (scoreDolphins > scoreKoalas) {
+//   console.log('Dolphins win the trophy');
+// } else if (scoreKoalas > scoreDolphins) {
+//   console.log('Koalas win the trophy');
+// } else if (scoreDolphins === scoreKoalas) {
+//   console.log('Both win the trophy');
+// }
+//  */
+
+
+// /* let day = 'saturday';
+
+//  switch (day) {
+//     case "monday":
+//         console.log("plan course structure");
+//         console.log("go to coding meet");
+//         break;
+//     case 'tuesday':
+//         console.log("prepare training videos");
+//         break;
+//     case 'wednesday':
+//     case 'thrusday':
+//         console.log("write code examples");
+//         break;
+//     case 'friday':
+//         console.log("record videos");
+//         break;
+//     case 'saturday':
+//     case 'sunday':
+//         console.log("Enjoy the weekend");
+//         break;
+//     default:
+//         console.log("Not a valid day");
+
+// }  */
+
+// let day = 'saturday';
+// if (day === 'monday') {
+//     console.log("plan course structure");
+//     console.log("go to coding meet");
+// } else if (day === 'tuesday') {
+//     console.log("prepare training videos");
+// } else if (day === 'wednesday' || day === 'thursday') {
+//     console.log(console.log("write code examples"));
+// } else if (day === 'friday') {
+//     console.log(console.log("record videos"));
+// } else if (day === 'saturday' || day === 'sunday') {
+//     console.log("Enjoy the weekend");
+// } else {
+//     console.log("Not a valid day");
+
+// }
+/* const age = 13;
+age >= 18 ? console.log("I'd like to drink 🍷") :
+    console.log("I'd like to drink 🚰"); */
+/* const age = 13;
+const drink = age >= 18 ? 'wine🍷' : 'water🚰';
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+    drink2 = 'wine🍷';
+} else {
+    drink2 = 'water🚰';
+}
+console.log(drink2);
+
+console.log(`i'd like to drink ${age >= 18 ? 'wine🍷' : 'water🚰'}`); */
+
+// const bill = 430;
+// const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.20;
+// console.log(`The bill was ${bill} and he tip was ${tip} and the total bill is ${bill + tip}`);
